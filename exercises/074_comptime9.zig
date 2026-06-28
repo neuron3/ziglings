fe4937b0bfbfbd9927462b19cb2b30bf658322be
neuron3 <quellkrafts@gmail.com>
2026-06-28 15:52:08 -0600

    checkpoint: did some advanced quizzes and exercises

--- exercises/074_comptime9.zig
+++ exercises/074_comptime9.zig
@@ -28,11 +28,11 @@ fn makeCreature(comptime count: usize, comptime fmt: []const u8) [count]Animal {
         start, // Ready to start a new animal.
         l, // This means we've seen an "l", so if we see an "m", we know it's a Llama.
     };
-    var state = State.start;
+    comptime var state = State.start;
 
     // We return an array of animals representing the creature. (This is why we
     // really needed the 'count' parameter. Arrays need a size.)
-    var animals: [count]Animal = undefined;
+    comptime var animals: [count]Animal = undefined;
     var next_animal: usize = 0;
 
     inline for (fmt) |char| {
@@ -57,7 +57,10 @@ fn makeCreature(comptime count: usize, comptime fmt: []const u8) [count]Animal {
                 //
                 // What do you think happens with Gators? Do they join with
                 // other animals or is this an error?
-                'g' => ???,
+                'g' => {
+                    animals[next_animal] = .Gator;
+                    next_animal;
+                },
 
                 else => @compileError(std.fmt.comptimePrint("No animal starts with '{c}'!", .{char})),
             },
@@ -69,7 +72,7 @@ fn makeCreature(comptime count: usize, comptime fmt: []const u8) [count]Animal {
                     next_animal += 1;
                     // Something is missing here. After we finish a Llama, we
                     // need to be ready to _start_ over with a new animal...
-                    ???
+                    state = .start;
                 },
 
                 else => @compileError("Only llamas start with 'l'!"),
@@ -100,7 +103,7 @@ pub fn main() void {
     //
     // You can solve this by adding "comptime" to two of the variables in
     // makeCreature...
-    const creature = makeCreature(2, "mlm");
+    const creature = comptime makeCreature(2, "mlm");
 
     for (creature) |animal| {
         // @tagName gives us a string representing which variant of an enum we
